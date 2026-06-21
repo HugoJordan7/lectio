31e4814d4e2ef6cefd5cb105adf799ad6b7eef28
Hugo Jordan <hugoj8366@gmail.com>
2026-06-20 21:23:39 -0300

    feat: migrate postgres database for sqlite

--- api/src/di/dependencyInjector.js
+++ api/src/di/dependencyInjector.js
@@ -1,29 +1,29 @@
-import UserDAOPostgres from '../dao/postgre/userDAOPostgreSQL.js';
+import UserDAOSQLite from '../dao/sqlite/userDAOSQLite.js';
 import UserService from '../services/userService.js';
 import UserController from '../controllers/userController.js';
-import BookDAOPostgreSQL from '../dao/postgre/bookDAOPostgreSQL.js';
+import BookDAOSQLite from '../dao/sqlite/bookDAOSQLite.js';
 import BookService from '../services/bookService.js';
 import BookController from '../controllers/bookController.js';
-import ReadingProgressDAOPostgreSQL from '../dao/postgre/readingProgressDAOPostgreSQL.js';
+import ReadingProgressDAOSQLite from '../dao/sqlite/readingProgressDAOSQLite.js';
 import ReadingProgressService from '../services/readingProgressService.js';
 import ReadingProgressController from '../controllers/readingProgressController.js';
-import CategoryDAOPostgreSQL from '../dao/postgre/categoryDAOPostgreSQL.js';
+import CategoryDAOSQLite from '../dao/sqlite/categoryDAOSQLite.js';
 import CategoryService from '../services/categoryService.js';
 import CategoryController from '../controllers/categoryController.js';
 
-const userDAO = new UserDAOPostgres();
+const userDAO = new UserDAOSQLite();
 const userService = new UserService(userDAO);
 const userController = new UserController(userService);
 
-const bookDAO = new BookDAOPostgreSQL();
+const bookDAO = new BookDAOSQLite();
 const bookService = new BookService(bookDAO, userDAO);
 const bookController = new BookController(bookService);
 
-const readingProgressDAO = new ReadingProgressDAOPostgreSQL();
+const readingProgressDAO = new ReadingProgressDAOSQLite();
 const readingProgressService = new ReadingProgressService(readingProgressDAO, userDAO, bookDAO);
 const readingProgressController = new ReadingProgressController(readingProgressService);
 
-const categoryDAO = new CategoryDAOPostgreSQL();
+const categoryDAO = new CategoryDAOSQLite();
 const categoryService = new CategoryService(categoryDAO, bookDAO, userDAO);
 const categoryController = new CategoryController(categoryService);
 
